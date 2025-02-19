cbe8c1ec0fa21d385f7e5976d88a95a416f281ba
Alice Barros Pereira <103052000+AliceBPereira@users.noreply.github.com>
2025-02-19 08:48:47 -0300

    feat: implementing models entities

---
+++ src/model/entities/inscritos.py
@@ -0,0 +1,11 @@
+from src.model.configs.base import Base
+from sqlalchemy import Integer, String, Column, ForeignKey
+
+
+class Inscritos(Base):
+    __tablename__ = "Inscritos"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    emial = Column(String, nullable=False)
+    link = Column(String)
+    evento_id =  Column(Integer, ForeignKey("Eventos.id"))
